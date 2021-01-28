07e8a9589813b60d7972149f8231327647f7f4ec
Prem Agrawal <11preamagrawal@gmail.com>
2021-01-28 20:45:43 +0530

    Updated Code
    This is Working Code

--- code.py
+++ code.py
@@ -5,22 +5,20 @@ from selenium import webdriver
 import time
 def fno_list():
     df = pd.read_csv("https://www1.nseindia.com/content/fo/fo_mktlots.csv")
+    df.to_csv()
     df = df.drop(df.index[3])
     fno_list_data = df.iloc[:, 1].to_list()
     fno_list_data = [x.strip(' ') for x in fno_list_data]
-    fno_list_data.remove('Symbol')
     return fno_list_data
-driver = webdriver.Chrome('chromedriver')
+driver = webdriver.Chrome('/Users/premagrawal/Downloads/chromedriver')
 driver.maximize_window()
 driver.get('https://www.tradingview.com/#signin')
-time.sleep(1)
+time.sleep(2)
 driver.find_element_by_xpath('//*[@id="overlap-manager-root"]/div/div[2]/div/div/div/div/div/div/div[1]/div[4]/div/span').click()
-username = ''
-password = ''
-driver.find_element_by_xpath('/html/body/div[10]/div/div[2]/div/div/div/div/div/div/form/div[1]/div[1]/input').send_keys()
-driver.find_element_by_xpath('/html/body/div[10]/div/div[2]/div/div/div/div/div/div/form/div[2]/div[1]/input').send_keys()
+driver.find_element_by_name("username").send_keys()
+driver.find_element_by_name("password").send_keys()
 driver.find_element_by_xpath("//button[@type='submit']").click()
-time.sleep(5)
+time.sleep(2)
 driver.find_element_by_link_text('Chart').click()
 time.sleep(5)
 driver.find_element_by_xpath('/html/body/div[2]/div[5]/div/div[2]/div/div/div/div/div[1]/div[2]').click()
@@ -31,10 +29,14 @@ for symbol in fno_list():
     symbol = symbol.replace('&','_')
     symbol = symbol.replace('-','_')
     symbol = 'NSE:' + str(symbol)
-    driver.find_element_by_xpath('/html/body/div[7]/div/div/div[2]/div/div[2]/div/input').send_keys(symbol)
-    time.sleep(1)
-    driver.find_element_by_xpath('/html/body/div[7]/div/div/div[2]/div/div[2]/div/input').send_keys(Keys.ENTER)
-    time.sleep(1)
-    driver.find_element_by_xpath('/html/body/div[2]/div[5]/div/div[1]/div[1]/div[1]/div[1]/div[1]/div/div[2]/span').click()
+    try :
+        driver.find_element_by_xpath('//*[@id="overlap-manager-root"]/div/div/div[2]/div/div[2]/div/input').send_keys(symbol)
+        time.sleep(1)
+        driver.find_element_by_xpath('//*[@id="overlap-manager-root"]/div/div/div[2]/div/div[2]/div/input').send_keys(Keys.ENTER)
+        time.sleep(1)
+        driver.find_element_by_xpath('/html/body/div[2]/div[5]/div/div[1]/div[1]/div[1]/div[1]/div[1]/div/div[2]/span').click()
+    except :
+        print(symbol)
+        pass
 time.sleep(5)
 driver.quit()
